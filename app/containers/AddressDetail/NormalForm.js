b99cb553305b6cd9f0aeaa3fec8b8e244721cb43
dnson <dnson272@gmail.com>
2017-07-22 05:07:50 +0700

    Add Validation rules

--- app/containers/AddressDetail/NormalForm.js
+++ app/containers/AddressDetail/NormalForm.js
@@ -7,6 +7,7 @@ const FormItem = Form.Item
 const Wrapper = styled(Col)`
   padding-top: 15px
 `
+
 const NormalForm = ({
   form,
   formItemLayout,
@@ -15,39 +16,61 @@ const NormalForm = ({
   district,
   city,
   country,
-}) =>
-  <Wrapper xs={24} md={12}>
-    <FormItem {...formItemLayout} label='Street Name'>
-      {form.getFieldDecorator('streetName', {
-        initialValue: streetName,
-      })(<Input placeholder='Street Name' id='streetName' />)}
-    </FormItem>
+}) => {
+  const checkCityExist = (rule, value, callback) => {
+    if (!(form.getFieldValue('city') || value)) {
+      callback(`Please input the ${rule.field}`)
+    }
+    else {
+      callback()
+    }
+  }
+  return (
+    <Wrapper xs={24} md={12}>
+      <FormItem {...formItemLayout} label='Street Name'>
+        {form.getFieldDecorator('streetName', {
+          initialValue: streetName,
+          rules: [
+            {
+              required: true,
+              message: 'Please input the street name'
+            }
+          ]
+        })(<Input placeholder='Street Name' />)}
+      </FormItem>
 
-    <FormItem {...formItemLayout} label='Ward' hasFeedback>
-      {form.getFieldDecorator('ward', {
-        initialValue: ward,
-      })(<Input placeholder='Ward' id='validating' />)}
-    </FormItem>
+      <FormItem {...formItemLayout} label='Ward' hasFeedback>
+        {form.getFieldDecorator('ward', {
+          initialValue: ward,
+          rules: [
+            {validator: checkCityExist}
+          ]
+        })(<Input placeholder='Ward' />)}
+      </FormItem>
 
-    <FormItem {...formItemLayout} label='District' hasFeedback>
-      {form.getFieldDecorator('district', {
-        initialValue: district,
-      })(<Input placeholder='District' />)}
-    </FormItem>
+      <FormItem {...formItemLayout} label='District' hasFeedback>
+        {form.getFieldDecorator('district', {
+          initialValue: district,
+          rules: [
+            {validator: checkCityExist}
+          ]
+        })(<Input placeholder='District' />)}
+      </FormItem>
 
-    <FormItem {...formItemLayout} label='City' hasFeedback>
-      {form.getFieldDecorator('city', {
-        initialValue: city,
-      })(<Input placeholder='City' />)}
-    </FormItem>
+      <FormItem {...formItemLayout} label='City' hasFeedback>
+        {form.getFieldDecorator('city', {
+          initialValue: city,
+        })(<Input placeholder='City' />)}
+      </FormItem>
 
-    <FormItem {...formItemLayout} label='Country' hasFeedback>
-      {form.getFieldDecorator('country', {
-        initialValue: country,
-      })(<Input placeholder='Country' />)}
-    </FormItem>
-  </Wrapper>
-
+      <FormItem {...formItemLayout} label='Country' hasFeedback>
+        {form.getFieldDecorator('country', {
+          initialValue: country,
+        })(<Input placeholder='Country' />)}
+      </FormItem>
+    </Wrapper>
+  )
+}
 NormalForm.propTypes = {
   form: PropTypes.object.isRequired,
   formItemLayout: PropTypes.object.isRequired,
